8dc5753b5ba402a6b4cc4719f0b7fddd806e7ecd
Clint Plummer <clint@yuhonas.com>
2022-12-18 15:53:14 -0600

    WIP

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,12 +1,13 @@
 locals {
   application = "spotter"
+  environment = "staging"
 }
 
 provider "aws" {
   default_tags {
     tags = {
       Application = local.application
-      Environment = "staging"
+      Environment = local.environment
       ProvisionedBy = "terraform"
     }
   }
@@ -14,50 +15,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# module "module" {
-#   source  = "hashicorp/module/random"
-#   version = "1.0.0"
-# }
-#
-# resource "random_password" "password" {
-#   length           = 16
-#   special          = true
-#   override_special = "!#$%&*()-_=+[]{}<>:?"
-# }
-#
-# resource "aws_db_instance" "default" {
-#   allocated_storage    = 10
-#   db_name              = "mydb"
-#   engine               = "mysql"
-#   engine_version       = "5.7"
-#   instance_class       = "db.t3.micro"
-#   username             = "foo"
-#
-#   # NOTE: The password will be stored in plaintext in the terraform state file, a solution using key management
-#   # would be a more secure solution if that is important
-#   password             = random_password.password.result
-#
-#   parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot  = true
-# }
-#
-#
-# output "database_identifier" {
-#   value = aws_db_instance.default.id
-# }
-#
-# output "database_password" {
-#   value = aws_db_instance.default.password
-#   sensitive = true
-# }
+module "module" {
+  source  = "hashicorp/module/random"
+  version = "1.0.0"
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
 
 resource "aws_elastic_beanstalk_application" "app" {
-  name        = local.application
-  # description = "tf-test-desc"
+  name                = "${local.application}-${local.environment}"
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = local.application
+  name                = "${local.application}-${local.environment}"
   application         = aws_elastic_beanstalk_application.app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.2 running Python 3.8"
 
@@ -66,9 +40,61 @@ resource "aws_elastic_beanstalk_environment" "env" {
       name = "IamInstanceProfile"
       value = "aws-elasticbeanstalk-ec2-role"
   }
-}
 
+  setting {
+      namespace = "aws:rds:dbinstance"
+      name      = "DBAllocatedStorage"
+      value     = "5"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBDeletionPolicy"
+    value     = "Delete"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "HasCoupledDatabase"
+    value     = "true"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBEngine"
+    value     = "postgres"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBEngineVersion"
+    value     = "14"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBInstanceClass"
+    value     = "db.t3.micro"
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBPassword"
+    value     = random_password.password.result
+  }
+
+  setting {
+    namespace = "aws:rds:dbinstance"
+    name      = "DBUser"
+    value     =  local.application
+  }
+}
 
 output "beanstalk_application_name" {
   value = aws_elastic_beanstalk_application.app.name
+}
+
+output "database_password" {
+  value = random_password.password.result
+  sensitive = true
 }
